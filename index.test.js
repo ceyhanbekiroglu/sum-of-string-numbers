2717387cb573a6eaec5f5b3b2d51c470bb5988a7
ceyhanbekiroglu <ceyhanbekiroglu@gmail.com>
2021-10-23 17:55:10 +0300

    Test fix

--- index.test.js
+++ index.test.js
@@ -1,14 +1,14 @@
 const sumFromString = require("./index");
 
 it("Test 1: Should calculate the sum of the numbers inside the string", () => {
-	const sum = sumFromString("i was born in 10/10/1989");
-	expect(sum).toEqual(2009);
+	const sum = sumFromString("Capt. Jack Sparrow was born in 10/10/1789");
+	expect(sum).toEqual(1809);
 });
 
 it("Test 2: Should calculate the sum of the numbers inside the string", () => {
 	const sum2 = sumFromString(
-		`Samsung s21 ultra has 3 cameras which are 108MP main camera with 24 mm-equivalent, 12MP ultra-wide with 13 mm-equivalent, 
+		`Samsung s21 ultra has 4 cameras which are 108MP main camera with 24 mm-equivalent, 12MP ultra-wide with 13 mm-equivalent, 
         10MP telephoto1 with 72 mm-equivalent, 10MP telephoto2 with 240 mm-equivalent  `
 	);
-	expect(sum2).toEqual(516);
+	expect(sum2).toEqual(517);
 });
